1d11f9f08d83ad8864401f2741e5c896d1d2ccf9
pranayjoshi <pranayjoshi4466@gmail.com>
2024-03-30 23:55:02 -0500

    added

--- main.js
+++ main.js
@@ -14,6 +14,33 @@ function createWindow() {
       nodeIntegration: true
     },
   });
+  mainWindow.webContents.on(
+    "select-bluetooth-device",
+    (event, deviceList, callback) => {
+      event.preventDefault();
+      console.log(deviceList);
+      deviceList.map((x) => {
+        console.log(x.deviceName);
+      });
+      //selectBluetoothCallback = callback
+
+      const result = deviceList.find((device) => {
+        return device.deviceName === MUSE_DEVICE_NAME;
+      });
+
+      //console.log(MuseClient)
+
+      if (result) {
+        callback(result.deviceId);
+
+        // Do we listen to characteristics here?
+        // Find examples of subscribing to characteristics in electron.js
+      } else {
+        // The device wasn't found so we need to either wait longer (eg until the
+        // device is turned on) or until the user cancels the request
+      }
+    }
+  );
 
   win.loadFile('index.html');
     // win.webContents.openDevTools();
--- preload.js
+++ preload.js
@@ -1,1 +1,12 @@
-window.require = require;
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  manualControl: (command) => ipcRenderer.send("manual-control", command),
+  controlSignal: (response) => ipcRenderer.send("control-signal", response),
+  cancelBluetoothRequest: (callback) =>
+    ipcRenderer.send("cancel-bluetooth-request", callback),
+  bluetoothPairingRequest: (callback) =>
+    ipcRenderer.on("bluetooth-pairing-request", callback),
+  bluetoothPairingResponse: (response) =>
+    ipcRenderer.send("bluetooth-pairing-response", response),
+});
